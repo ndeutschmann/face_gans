81d8e0df2e4d37f9dc1f72bd0e3f7bdd5e1477cc
Nicolas DEUTSCHMANN <ndeutschmann@hotmail.com>
2020-05-23 23:21:59 +0200

    tweaked inversion model architecture

--- src/models/dcgan32/dcgan32_inversion.py
+++ src/models/dcgan32/dcgan32_inversion.py
@@ -12,30 +12,29 @@ class DCGAN32Inverter(torch.nn.Module):
             torch.nn.Conv2d(3, channels, 3, 1, 1),
             torch.nn.BatchNorm2d(channels),
             torch.nn.LeakyReLU(),
+            torch.nn.Dropout(dropout_rate),
             torch.nn.Conv2d(channels, channels * 2, 3, 2, 1),
             torch.nn.Dropout(dropout_rate),
             torch.nn.LeakyReLU(),
+            torch.nn.Dropout(dropout_rate),
             torch.nn.Conv2d(channels * 2, channels * 4, 2, 2, 1),
             torch.nn.BatchNorm2d(channels * 4),
             torch.nn.LeakyReLU(),
-            torch.nn.Conv2d(channels * 4, channels * 8, 2, 2),
             torch.nn.Dropout(dropout_rate),
+            torch.nn.Conv2d(channels * 4, channels * 8, 2, 2),
             torch.nn.LeakyReLU(),
+            torch.nn.Dropout(dropout_rate),
             torch.nn.Conv2d(channels * 8, channels * 16, 2, 4),
             torch.nn.BatchNorm2d(channels * 16),
             torch.nn.LeakyReLU(),
+            torch.nn.Dropout(dropout_rate),
         )
 
         self.dense = torch.nn.Sequential(
-            torch.nn.Linear(16 * channels, 16 * channels),
-            torch.nn.BatchNorm1d(channels * 16),
-            torch.nn.Dropout(dropout_rate),
+            torch.nn.Linear(16 * channels, 200),
+            torch.nn.BatchNorm1d(200),
             torch.nn.LeakyReLU(),
-            torch.nn.Linear(16 * channels, 16 * channels),
-            torch.nn.BatchNorm1d(channels * 16),
-            torch.nn.Dropout(dropout_rate),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(16 * channels, 100)
+            torch.nn.Linear(200, 100)
         )
 
     def forward(self, x):
